4713f7eaf08c36b6c48020f00b4b2c28b4477c0e
Kajsa Asp <kajsalisaasp@gmail.com>
2024-11-04 11:56:05 +0100

    added tailwind and shadcn

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+import path from "path"
+
+// Config för att kunna definiera __dirname https://iamwebwiz.medium.com/how-to-fix-dirname-is-not-defined-in-es-module-scope-34d94a86694d
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,5 +16,10 @@ export default defineConfig({
     proxy: {
       '/api': 'http://localhost:3000'
     }
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
