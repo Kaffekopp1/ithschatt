c6156194cf7e44bf2a62530a3d123adffc2fed2a
Christopher Rydberg <christopher.rydberg@gmail.com>
2024-11-05 16:25:38 +0100

    added some basics to SignupForm

--- frontend/src/components/SignupForm.jsx
+++ frontend/src/components/SignupForm.jsx
@@ -1,3 +1,45 @@
-const CreateAccount = () => <h1>Create Account Page</h1>;
+// import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import { Input } from '@/components/ui/input';
+import { useForm } from 'react-hook-form';
 
-export default CreateAccount;
+export default function SignupForm() {
+  const form = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input placeholder="shadcn" {...field} />
+                </FormControl>
+                <FormDescription>
+                  This will be your public chat name.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
+}
