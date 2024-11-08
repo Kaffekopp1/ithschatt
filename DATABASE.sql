55818ad53e0bae7dc200f97fb973af8c193c6948
Peter Zemturis <pzemturis@gmail.com>
2024-11-08 12:49:39 +0100

    added first_name and last_name

--- DATABASE.sql
+++ DATABASE.sql
@@ -1,6 +1,8 @@
 CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     username VARCHAR(50) UNIQUE NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
     password_hash VARCHAR(255) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
