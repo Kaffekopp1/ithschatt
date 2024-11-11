3f83a9556ff13216bfb99f928345cbbbe278a531
Peter Zemturis <pzemturis@gmail.com>
2024-11-11 11:46:25 +0100

    prettier test

--- backend/index.js
+++ backend/index.js
@@ -1,47 +1,47 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const { Server } = require("socket.io");
-const http = require("http");
+const express = require('express');
+const dotenv = require('dotenv');
+const { Server } = require('socket.io');
+const http = require('http');
 dotenv.config();
 
 const app = express(),
-  port = 3000;
+	port = 3000;
 
-const cors = require("cors");
+const cors = require('cors');
 const server = http.createServer(app);
 
 app.use(cors());
 app.use(express.json());
 
 const io = new Server(server, {
-	cors: { origin: "http://localhost:5173", methods: ["GET", "POST"] }
+	cors: { origin: 'http://localhost:5173', methods: ['GET', 'POST'] },
 });
 
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
 	console.log(`A user connected: ${socket.id}`);
-	socket.on("send_message", (data) => {
-		console.log("data", data);
-		socket.broadcast.emit("receive_message", data);
+	socket.on('send_message', (data) => {
+		console.log('data', data);
+		socket.broadcast.emit('receive_message', data);
 	});
 
-	socket.on("disconnect", () => {
+	socket.on('disconnect', () => {
 		console.log(`User disconnected: ${socket.id}`);
 	});
 });
 
-const messageRoutes = require("./routes/messageRoutes");
-const authRoutes = require("./routes/authRoutes");
-const protectedRoutes = require("./routes/protectedRoutes");
+const messageRoutes = require('./routes/messageRoutes');
+const authRoutes = require('./routes/authRoutes');
+const protectedRoutes = require('./routes/protectedRoutes');
 app.use(messageRoutes);
 app.use(authRoutes);
 app.use(protectedRoutes);
 
-app.get("/", (_request, response) => {
-  response.json({ hello: "Världen" });
+app.get('/', (_request, response) => {
+	response.json({ hello: 'Världen' });
 });
 server.listen(4000, () => {
-	console.log("Listening on *:4000");
+	console.log('Listening on *:4000');
 });
 app.listen(port, () => {
-  console.log(`Ready on http://localhost:${port}/`);
+	console.log(`Ready on http://localhost:${port}/`);
 });
--- frontend/src/api/authFetch.jsx
+++ frontend/src/api/authFetch.jsx
@@ -1,22 +1,22 @@
 export async function updateUserStatus(userId) {
-  try {
-    const response = await fetch("http://localhost:3000/updateUserStatus", {
-      method: "PATCH",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ user_id: userId }),
-    });
+	try {
+		const response = await fetch('http://localhost:3000/updateUserStatus', {
+			method: 'PATCH',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({ user_id: userId }),
+		});
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to update status: ${response.status} ${response.statusText}`
-      );
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error("Error updating user status:", error);
-    throw error;
-  }
+		if (!response.ok) {
+			throw new Error(
+				`Failed to update status: ${response.status} ${response.statusText}`
+			);
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error updating user status:', error);
+		throw error;
+	}
 }
