844e3a15cee5ef0b1077780a06d79b4f1abf9418
Peter Zemturis <pzemturis@gmail.com>
2024-11-13 15:45:10 +0100

    merge fix

--- frontend/src/api/authFetch.jsx
+++ frontend/src/api/authFetch.jsx
@@ -1,14 +1,4 @@
 export async function updateUserStatus(userId) {
-<<<<<<< HEAD
-  try {
-    const response = await fetch('/api/updateUserStatus', {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ user_id: userId }),
-    });
-=======
 	try {
 		const response = await fetch('http://localhost:3000/api/updateUserStatus', {
 			method: 'PATCH',
@@ -17,17 +7,16 @@ export async function updateUserStatus(userId) {
 			},
 			body: JSON.stringify({ user_id: userId }),
 		});
->>>>>>> 8b2971d (changes in localhost api)
 
-    if (!response.ok) {
-      throw new Error(
-        `Failed to update status: ${response.status} ${response.statusText}`
-      );
-    }
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.error('Error updating user status:', error);
-    throw error;
-  }
+		if (!response.ok) {
+			throw new Error(
+				`Failed to update status: ${response.status} ${response.statusText}`
+			);
+		}
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.error('Error updating user status:', error);
+		throw error;
+	}
 }
