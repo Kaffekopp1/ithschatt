17c43e5b14e573b5db3c216694f186fc48187c06
Kajsa Asp <kajsalisaasp@gmail.com>
2024-11-14 16:27:23 +0100

    fix edit profile usecontext userinfo

--- frontend/src/pages/ImageGallery.jsx
+++ frontend/src/pages/ImageGallery.jsx
@@ -2,9 +2,9 @@ import { useNavigate } from 'react-router-dom';
 import AuthContext from '../AuthContext';
 import { useContext } from 'react';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
-// import image1 from './assets/images/image1';
 import image1 from '../assets/images/image1.jpg';
 import styles from './ImageGallery.module.css';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 export default function ImageGallery() {
 	const navigate = useNavigate();
@@ -19,11 +19,12 @@ export default function ImageGallery() {
 			<p>Hi how are you {user}, look at amazing pictures:</p>
 			<LazyLoadImage
 				className={styles.imageWrapper}
-				alt="Beskrivande text för bilden"
+				alt="Bild på användare"
 				height={800}
 				src={image1}
 				width={300}
-				// effect="blur"
+				loading="lazy"
+				effect="blur"
 			/>
 			<p>Navigate to chat:</p>
 			<button onClick={test}>Chat</button>
